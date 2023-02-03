c669a37ad3ad86808b2d4002090bd4fb47c4e76f
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-02-03 11:41:09 -0800

    chore: add copyright

--- dfu-tutorials/esp8266/esp8266-dfu-v1.1.0.0/esp8266-dfu/dfu-esp32.h
+++ dfu-tutorials/esp8266/esp8266-dfu-v1.1.0.0/esp8266-dfu/dfu-esp32.h
@@ -1,3 +1,7 @@
+//
+// Copyright 2023 Blues Inc.  All rights reserved.
+// Use of this source code is governed by licenses granted by the
+// copyright holder including that found in the LICENSE file.
 #pragma once
 
 #if defined(ARDUINO_ARCH_ESP32) || 1
